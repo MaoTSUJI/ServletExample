b7352a525700f1d794ff7c4aef25a61642c1d676
辻真緒 <>
2020-01-05 14:47:44 +0900

    モデル　Health.javaを作成

--- src/model/Health.java
+++ src/model/Health.java
@@ -2,4 +2,23 @@ package model;
 
 public class Health {
 
+	private double height, weight, bmi;
+	private String bodyType;
+
+	public double getHeight() { return height; }
+	public void setHeight(double height) { this.height = height; }
+
+	public double getWeight() { return weight; }
+	public void setWeight(double weight) { this.weight = weight; }
+
+	public double getBmi() { return bmi; }
+	public void setBmi(double bmi) { this.bmi = bmi; }
+
+	public void setBodyType(String bodyType) {
+		this.bodyType = bodyType;
+	}
+	public String getBodyType() {
+		return bodyType;
+	}
+
 }
